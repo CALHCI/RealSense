82ef5d9d024a7b5a1aa2f6290e706a8d72c71c3e
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2024-02-21 17:33:52 -0800

    Updated to have all colors of hanoi tower

--- cam.py
+++ cam.py
@@ -1,92 +1,165 @@
-import cv2
+import sys
 import numpy as np
+import cv2
 
-# Start capturing video from the webcam
+
+
 webcam = cv2.VideoCapture(0)
+    
+while True: 
+    
 
-while True:
-    # Read frames from the webcam
-    _, imageFrame = webcam.read()
 
-    # Convert the frame to HSV color space
-    hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
+    _, imageFrame = webcam.read() 
 
-    # Define HSV ranges for detecting colors
+    #convert imageFrame to HSV (hue-saturation-value)
+    hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV) 
 
-    # Red color
-    red_lower1 = np.array([0, 120, 70], np.uint8)
-    red_upper1 = np.array([10, 255, 255], np.uint8)
-    red_lower2 = np.array([160, 120, 70], np.uint8)
-    red_upper2 = np.array([180, 255, 255], np.uint8)
-    red_mask1 = cv2.inRange(hsvFrame, red_lower1, red_upper1)
-    red_mask2 = cv2.inRange(hsvFrame, red_lower2, red_upper2)
-    red_mask = red_mask1 + red_mask2
+    #define the range of blue in HSV
+    blue_lower = np.array([94, 80, 2], np.uint8) 
+    blue_upper = np.array([120, 255, 255], np.uint8) 
+    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
+      
+    #define the range of red in HSV
+    red_lower = np.array([136, 87, 111], np.uint8) 
+    red_upper = np.array([180, 255, 255], np.uint8) 
+    red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
 
-    # Blue color
-    blue_lower = np.array([94, 80, 2], np.uint8)
-    blue_upper = np.array([120, 255, 255], np.uint8)
-    blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
+    kernel = np.ones((5, 5), "uint8") 
 
-    # Yellow color
+    #define the range of yellow in HSV
     yellow_lower = np.array([22, 93, 0], np.uint8)
-    yellow_upper = np.array([38, 255, 255], np.uint8)
-    yellow_mask = cv2.inRange(hsvFrame, yellow_lower, yellow_upper)
+    yellow_upper = np.array([45, 255, 255], np.uint8)
+    yellow_mask = cv2.inRange(hsvFrame, yellow_lower, yellow_upper) 
 
-    # Green color
-    green_lower = np.array([36, 50, 70], np.uint8)
-    green_upper = np.array([86, 255, 255], np.uint8)
-    green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
+    #define the range of green in HSV
+    lower_green = np.array([50,100,50], np.uint8)
+    upper_green = np.array([70,255,255], np.uint8)
+    green_mask = cv2.inRange(hsvFrame, lower_green, upper_green)
 
-    # Morphological Transform, Dilation for each color
-    kernel = np.ones((5, 5), "uint8")
-    red_mask = cv2.dilate(red_mask, kernel)
-    blue_mask = cv2.dilate(blue_mask, kernel)
-    yellow_mask = cv2.dilate(yellow_mask, kernel)
-    green_mask = cv2.dilate(green_mask, kernel)
+    #define the range of purple in HSV
+    lower_purple = np.array([130, 0, 0], np.uint8)
+    upper_purple = np.array([150, 255,255], np.uint8)
+    purple_mask = cv2.inRange(hsvFrame, lower_purple, upper_purple)
 
-    # Tracking the Red Color
-    contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > 300:
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            cv2.putText(imageFrame, "Red Color", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255))
 
-    # Tracking the Blue Color
-    contours, _ = cv2.findContours(blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > 300:
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.putText(imageFrame, "Blue Color", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0))
 
-    # Tracking the Yellow Color
-    contours, _ = cv2.findContours(yellow_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > 300:
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 255, 255), 2)
-            cv2.putText(imageFrame, "Yellow Color", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 255))
+    # Threshold the HSV image to get only blue colors
+    blue_mask = cv2.dilate(blue_mask, kernel) 
+    res_blue = cv2.bitwise_and(imageFrame, imageFrame, 
+                               mask = blue_mask) 
+    
+    # Threshold the HSV image to get only red colors
+    red_mask = cv2.dilate(red_mask, kernel) 
+    res_red = cv2.bitwise_and(imageFrame, imageFrame, 
+                               mask = red_mask) 
+    
+    # Threshold the HSV image to get only yellow colors
+    yellow_mask = cv2.dilate(yellow_mask, kernel) 
+    res_yellow = cv2.bitwise_and(imageFrame, imageFrame, 
+                               mask = yellow_mask) 
+    
+    # Threshold the HSV image to get only yellow colors
+    green_mask = cv2.dilate(green_mask, kernel) 
+    res_green = cv2.bitwise_and(imageFrame, imageFrame, 
+                               mask = green_mask) 
+    
+    # Threshold the HSV image to get only purple colors
+    purple_mask = cv2.dilate(purple_mask, kernel) 
+    res_purple = cv2.bitwise_and(imageFrame, imageFrame, 
+                               mask = purple_mask) 
 
-    # Tracking the Green Color
-    contours, _ = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > 300:
-            x, y, w, h = cv2.boundingRect(contour)
-            imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(imageFrame, "Green Color", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0))
+    
+    
+    # Creating contour to track blue color 
+    contours, hierarchy = cv2.findContours(blue_mask, 
+                                           cv2.RETR_TREE, 
+                                           cv2.CHAIN_APPROX_SIMPLE) 
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y), 
+                                       (x + w, y + h), 
+                                       (255, 0, 0), 2) 
+              
+            cv2.putText(imageFrame, "Blue Colour", (x, y), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 
+                        1.0, (255, 0, 0)) 
+            
+    # Creating contour to track red color 
+    contours, hierarchy = cv2.findContours(red_mask, 
+                                           cv2.RETR_TREE, 
+                                           cv2.CHAIN_APPROX_SIMPLE) 
+      
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y),  
+                                       (x + w, y + h),  
+                                       (0, 0, 255), 2) 
+              
+            cv2.putText(imageFrame, "Red Colour", (x, y), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+                        (0, 0, 255))   
 
-    # Display the resulting frame
-    cv2.imshow("Color Detection in Real-Time", imageFrame)
+    # Creating contour to track Yellow color 
+    contours, hierarchy = cv2.findContours(yellow_mask, 
+                                           cv2.RETR_TREE, 
+                                           cv2.CHAIN_APPROX_SIMPLE) 
+      
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y),  
+                                       (x + w, y + h),  
+                                       (0, 0, 255), 2) 
+              
+            cv2.putText(imageFrame, "Yellow Colour", (x, y), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+                        (0, 0, 255))  
 
-    # Break the loop when 'q' is pressed
-    if cv2.waitKey(10) & 0xFF == ord('q'):
+    # Creating contour to track Green color 
+    contours, hierarchy = cv2.findContours(green_mask, 
+                                           cv2.RETR_TREE, 
+                                           cv2.CHAIN_APPROX_SIMPLE) 
+      
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y),  
+                                       (x + w, y + h),  
+                                       (0, 0, 255), 2) 
+              
+            cv2.putText(imageFrame, "Green Colour", (x, y), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+                        (0, 0, 255))  
+            
+    # Creating contour to track Purple color 
+    contours, hierarchy = cv2.findContours(purple_mask, 
+                                           cv2.RETR_TREE, 
+                                           cv2.CHAIN_APPROX_SIMPLE) 
+      
+    for pic, contour in enumerate(contours): 
+        area = cv2.contourArea(contour) 
+        if(area > 300): 
+            x, y, w, h = cv2.boundingRect(contour) 
+            imageFrame = cv2.rectangle(imageFrame, (x, y),  
+                                       (x + w, y + h),  
+                                       (0, 0, 255), 2) 
+              
+            cv2.putText(imageFrame, "Purple Colour", (x, y), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
+                        (0, 0, 255))  
+         
+            
+    # Program Termination 
+    cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame) 
+    if cv2.waitKey(10) & 0xFF == ord('q'): 
+        webcam.release() 
+        cv2.destroyAllWindows() 
         break
 
-# Release the capture and destroy all OpenCV windows
-webcam.release()
-cv2.destroyAllWindows()
