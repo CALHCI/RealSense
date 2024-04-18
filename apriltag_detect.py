9240f2a1270dddb1691b45e0ee61e816f02cb7f9
Phillip <90676461+phillip-niakoulov@users.noreply.github.com>
2024-04-18 23:57:33 +0000

    Intel RealSense update

--- apriltag_detect.py
+++ apriltag_detect.py
@@ -1,16 +1,22 @@
 import apriltag
 import numpy as np
+import pyrealsense2 as rs
 import cv2
+import argparse
 import time
+
+class DetectionError(Exception):
+	"""Exception raised when the camera fails to detect an AprilTag"""
+	pass
 
 def draw_details(tag, img):
 	"""Modify image to draw a bounding box, center, and id number on top of
 	the april tags to show additional information.
 
-    Args:
-        tag (AprilTag): April tag object
+	Args:
+		tag (AprilTag): April tag object
 		img (Image): Opencv image object
-    """
+	"""
 	(ptA, ptB, ptC, ptD) = tag.corners
 	ptB = (int(ptB[0]), int(ptB[1]))
 	ptC = (int(ptC[0]), int(ptC[1]))
@@ -31,108 +37,165 @@ def draw_details(tag, img):
 				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
 
-def get_average_xyz(arr):
+def get_average_pos(arr):
 	"""Based on a given list of coordinates, find the average x, y, and z of a
 	every tag in the array.
 
-    Args:
-        arr (List): 2D List containing coordinates grouped by id
+	Args:
+		arr (List): 2D List containing coordinates grouped by id
 
-    Returns:
-        Tuple: A tuple which returns (x, y, z)
-    """
+	Returns:
+		Tuple: A tuple which returns (x, y, z)
+	"""
 	arr = np.array(arr)
 	return (np.mean(arr[:, 0]), np.mean(arr[:, 1]), np.mean(arr[:, 2]))
 
 
-def get_average_location_of_id(n, calibration_file, headless=True):
-	"""Take a picture and locate average location of each tag. So get the
-	average location of all tags with ID 0 and all tags with ID 1 and so on.
+def get_pos_of_dividers(pipe, reverse=False):
+	"""Return the x of both AprilTags with IDs of 0 in sorted order (or in reverse
+	sorted order depending on the camera's orientation).
 
-    Args:
-        n (int): Quantity of all tower of hanoi blocks.
-		calibration_file (string): File path to numpy calibration file
-		headless (boolean): True to hide debug image
+	Args:
+		pipe (PipeLine): Intel Realsense pipeline
+		reverse (boolean): Whether to output the boundaries in reverse
 
-    Returns:
-        List: List of tuples which store average (x, y, z) in the order of ID
-    """
+	Returns:
+		Tuple: A tuple which stores the x of both dividers in sorted order
+	"""
 	# Set up April Tag detector to work with "tag36h11" tags.
 	options = apriltag.DetectorOptions(families="tag36h11")
 	detector = apriltag.Detector(options)
 
-	# Set up the camera to take pictures.
-	cam = cv2.VideoCapture(0) # Change the camera number if errors occur.
-	if not cam.isOpened():
-		raise ValueError("Camera not found. Maybe change cam number?")
-
-	# Load numpy calibration results (loads fx, fy, cx, and cy).
-	calib_data = np.load(calibration_file)
-	mtx = [calib_data["mtx"][0][0],  # fx
-			calib_data["mtx"][1][1], # fy
-			calib_data["mtx"][0][2], # cx
-			calib_data["mtx"][1][2]  # cy
-			]
-
 	# Take a picture.
-	res, img = cam.read()
-	if not res: raise ValueError("Couldn't take picture.")
+	frame = pipe.wait_for_frames()
+	color_frame = frame.get_color_frame()
+	img = np.asanyarray(color_frame.get_data())
+
+	# Get calibration results.
+	intr = color_frame.profile.as_video_stream_profile().intrinsics
+	camera_matrix = np.array([[intr.fx, 0, intr.ppx],
+							[0, intr.fy, intr.ppy],
+							[0, 0, 1]])
+	dist_coeffs = np.array(intr.coeffs)
 
 	# Modify the image to undo warping and make grayscale version for detection.
-	img = cv2.undistort(img,calib_data["mtx"], calib_data["dist"], None,
-						calib_data["mtx"])
+	img = cv2.undistort(img, camera_matrix, dist_coeffs)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	found_tags = []
+
+	for april_tag in detector.detect(gray):
+		if april_tag.tag_id == 0:
+			# Get 3D pose of the AprilTag. Change tag_size to the tag size.
+			# e1 and e2 represent error (no idea how to use)
+			mtx = [intr.fx, intr.fy, intr.ppx, intr.ppy]
+			pose, e1, e2 = detector.detection_pose(april_tag, mtx, tag_size=1)
+
+			# Extract translation vectors from pose.
+			found_tags.append(pose[:-1, 3])
+	
+	if len(found_tags) < 2:
+		raise ValueError("Two AprilTag dividers not found.")
+	elif len(found_tags) > 2:
+		raise ValueError("Too many AprilTag dividers found.")
+	else:
+		if reverse:
+			return (max(found_tags[0][0], found_tags[1][0]),
+					min(found_tags[0][0], found_tags[1][0])
+				)
+		else:
+			return (min(found_tags[0][0], found_tags[1][0]),
+					max(found_tags[0][0], found_tags[1][0])
+				)
+	
+	
+
+
+def get_average_location_of_id(pipe, n, headless=True):
+	"""Take a picture and locate average location of each tag. So get the
+	average location of all tags with ID 1 and all tags with ID 2 and so on.
+
+	Args:
+		pipe (PipeLine): Intel Realsense pipeline
+		n (int): Quantity of all tower of hanoi blocks
+		headless (boolean): Hide debug image if true
+
+	Returns:
+		List: List of tuples which store average (x, y, z) in the order of ID
+	"""
+
+	# Set up April Tag detector to work with "tag36h11" tags.
+	options = apriltag.DetectorOptions(families="tag36h11")
+	detector = apriltag.Detector(options)
+
+	# Take a picture.
+	frame = pipe.wait_for_frames()
+	color_frame = frame.get_color_frame()
+	img = np.asanyarray(color_frame.get_data())
+
+	# Get calibration results (loads fx, fy, cx, and cy).
+	intr = color_frame.profile.as_video_stream_profile().intrinsics
+	camera_matrix = np.array([[intr.fx, 0, intr.ppx],
+							[0, intr.fy, intr.ppy],
+							[0, 0, 1]])
+	dist_coeffs = np.array(intr.coeffs)
+
+	# Modify the image to undo warping and make grayscale version for detection.
+	img = cv2.undistort(img, camera_matrix, dist_coeffs)
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 	# Stores all coordinates of each id.
 	coords = [[] for _ in range(n)]
 	
 	for april_tag in detector.detect(gray):
-		# Draw bounding box, center, and id on top of each tag.
-		if not headless: draw_details(april_tag, img)
+		if april_tag.tag_id <= n and april_tag.tag_id > 0:
+			# Draw bounding box, center, and id on top of each tag.
+			if not headless: draw_details(april_tag, img)
 
-		# Get 3D pose of the AprilTag. Change tag_size to the tag size.
-		# e1 and e2 represent error (no idea how to use)
-		pose, e1, e2 = detector.detection_pose(april_tag, mtx, tag_size=1)
+			# Get 3D pose of the AprilTag. Change tag_size to the tag size.
+			# e1 and e2 represent error (no idea how to use)
+			mtx = [intr.fx, intr.fy, intr.ppx, intr.ppy]
+			pose, e1, e2 = detector.detection_pose(april_tag, mtx, tag_size=1)
 
-		# Extract translation and rotation vectors from pose.
-		rvec, tvec = pose[:-1, :3], pose[:-1, 3]
+			# Extract translation and rotation vectors from pose.
+			rvec, tvec = pose[:-1, :3], pose[:-1, 3]
 
-		# Store translation vector in coordinates array at its id's list.
-		coords[april_tag.tag_id].append(tvec)
+			# Store translation vector in coordinates array at its id's list.
+			coords[april_tag.tag_id - 1].append(tvec)
 	
 	# Show image with bounding boxes.
 	if not headless: 
 		cv2.imshow("Out", img)
 		cv2.waitKey(1)
 
-	cam.release()
-
-	return [get_average_xyz(coord) for coord in coords if coord]
+	return [get_average_pos(coord) for coord in coords if coord]
 
 def get_hanoi_tower(arr, left_boundary, right_boundary):
 	"""From the tag locations, determine which rod the rings a part of and in
 	what order they're in to generate a tower of hanoi frame.
 
-    Args:
+	Args:
 		arr (List): List of each hanoi block location in 3d space in order of ID
 		left_boundary (double): x value which separates rod 1 and rod 2
 		right_boundary (double): x value which separates rod 3 and rod 2
 
-    Returns:
-        List: Rods and the rings on them in their corresponding order
-    """
+	Returns:
+		List: Rods and the rings on them in their corresponding order
+	"""
 	rod_1 = []
 	rod_2 = []
 	rod_3 = []
 
+	# Split locations into their rods based on their x value relative to the boundaries.
 	for i, coord in enumerate(arr):
 		if coord[0] < left_boundary:
-			rod_1.append((i, coord[1]))
+			rod_1.append((i + 1, coord[1]))
 		elif coord[0] > right_boundary:
-			rod_3.append((i, coord[1]))
+			rod_3.append((i + 1, coord[1]))
 		else:
-			rod_2.append((i, coord[1]))
+			rod_2.append((i + 1, coord[1]))
 
+	# Sort based on y value.
 	rod_1 = sorted(rod_1, key=lambda item: item[1], reverse=True)
 	rod_2 = sorted(rod_2, key=lambda item: item[1], reverse=True)
 	rod_3 = sorted(rod_3, key=lambda item: item[1], reverse=True)
@@ -145,11 +208,45 @@ def get_hanoi_tower(arr, left_boundary, right_boundary):
 
 
 if __name__ == "__main__":
-	# Repeatedly prints the average locations of the april tags by ID.
-	print(get_hanoi_tower(
-		get_average_location_of_id(4, "calibration_results.npz", False),
-		4.0, # PLAY WITH THIS NUMBER A TON (and maybe swap these two around?)
-		10.0 # PLAY WITH THIS NUMBER A TON
-	))
-	# while True:
-	# 	print(get_average_location_of_id(4, "calibration_results.npz", False))
+	# Set up args.
+	parser = argparse.ArgumentParser(description="Track AprilTags on a hanoi tower to \
+												return its digital state.")
+	parser.add_argument("--debug", action="store_true", help="enable debug mode")
+	parser.add_argument("n", type=int, help="amount of rings")
+	parser.add_argument("-d", "--delay", default=0, type=int, help="delay in \
+														milliseconds between message")
+	args = parser.parse_args()
+
+	# Set up Intel Realsense camera.
+	pipe = rs.pipeline()
+	cfg = rs.config()
+	cfg.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+	pipe.start(cfg)
+
+	# Get both x values of tags seperating the rods
+	try:
+		left_boundary, right_boundary = get_pos_of_dividers(pipe, reverse=False)
+	except Exception as e:
+		if args.debug:
+			left_boundary, right_boundary = 0, 0
+			print("Error: ", e)
+			print("Continuing with boundaries set to 0...")
+			time.sleep(3)
+		else:
+			raise DetectionError(e)
+
+	# Prints the state of the hanoi tower
+	while True:
+		try:
+			print(get_hanoi_tower(
+				get_average_location_of_id(pipe, args.n, not args.debug),
+				left_boundary,
+				right_boundary
+			))
+			time.sleep(args.delay / 1000.0)
+		except KeyboardInterrupt:
+			print("\nCtrl+C detected. Exiting...")
+			break
+	
+	pipe.stop()
+	cv2.destroyAllWindows()
