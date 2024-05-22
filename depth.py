618c6e816decd2a4f73e350a5b24236263208104
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2024-05-22 16:38:34 -0700

    Update depth.py with comments

--- depth.py
+++ depth.py
@@ -1,6 +1,13 @@
 import pyrealsense2 as rs
 import numpy as np
-import cv2
+import cv2 
+
+"""
+#Purpose: Attempted to in realtime track the distance of the camera to 3 random points on the webfeed.
+#Result: Successfully used camera depth sensing to accurately track the distance from the camera
+to 3 points that are randomly generated. Precursor to both.py where the distance to the center
+of the object is accurately tracked in realtime.
+"""
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
@@ -8,8 +15,28 @@ config = rs.config()
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
+#Video Capture
+cap = cv2.VideoCapture(1)
+ret, frame = cap.read()
+
 # Start streaming
 pipeline.start(config)
+
+
+#Select Region of Interest
+bbox = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
+
+#Create Tracker for object
+tracker = cv2.legacy.MultiTracker_create()
+tracker.add(cv2.legacy.TrackerCSRT_create(), frame, bbox)
+print("Multitracker created")
+
+
+
+#Draw a Bounding Box
+p1 = (int(bbox[0]), int(bbox[1]))
+p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+cv2.rectangle(frame, p1, p2, (0, 255, 0), 2, 1)
 
 try:
     while True:
@@ -17,6 +44,7 @@ try:
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
+        
         if not depth_frame or not color_frame:
             continue
 
